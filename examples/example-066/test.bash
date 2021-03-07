d03a2fe22aefad35f1e0c49d84c804ad01e9ff5d
getkub <kinbsk@gmail.com>
2021-03-07 15:15:20 +0000

    updated /tmp/

--- examples/example-066/test.bash
+++ examples/example-066/test.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook test-include_dir2.yml > /scratch/tmp/dir2.log &
-ansible-playbook test-include_dir.yml > /scratch/tmp/dir.log &
+ansible-playbook test-include_dir2.yml > /tmp/ansible_examples/dir2.log &
+ansible-playbook test-include_dir.yml > /tmp/ansible_examples/dir.log &
